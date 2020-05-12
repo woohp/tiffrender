c7d902367fd7c755ec6380014b9464d71418ff88
Marc Abonce Seguin <marca@vidado.ai>
2020-05-11 20:29:54 -0700

    fix dpi overflow

--- src/tiffrender.cpp
+++ src/tiffrender.cpp
@@ -84,7 +84,7 @@ public:
 
         auto pil_image = Image.attr("frombytes")("RGBA", size, buffer, "raw", "BGRA");
         auto info = pil_image.attr("info");
-        info["dpi"] = make_pair(int(dpix), int(dpiy));
+        info["dpi"] = make_pair(dpix, dpiy);
         delete[] image_data;
 
         return pil_image;
